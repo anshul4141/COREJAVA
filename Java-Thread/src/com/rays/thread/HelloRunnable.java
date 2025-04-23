7268685678c870c2b19923cadb0eae08fb8d3c69
anshul4141 <anshulprajapati691@gmail.com>
2025-04-23 11:58:45 +0530

    done

---
+++ Java-Thread/src/com/rays/thread/HelloRunnable.java
@@ -0,0 +1,20 @@
+package com.rays.thread;
+
+public class HelloRunnable implements Runnable {
+
+	String name;
+
+	public HelloRunnable(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " = " + name);
+		}
+
+	}
+
+}
