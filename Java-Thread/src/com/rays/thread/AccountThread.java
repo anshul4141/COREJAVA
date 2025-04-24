06156e84750ea7542ec640460deb3c38c98691ea
anshul4141 <anshulprajapati691@gmail.com>
2025-04-24 12:20:51 +0530

    done

---
+++ Java-Thread/src/com/rays/thread/AccountThread.java
@@ -0,0 +1,20 @@
+package com.rays.thread;
+
+public class AccountThread extends Thread {
+
+	String name;
+
+	public static Account a = new Account();
+
+	public AccountThread(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 1; i <= 5; i++) {
+			a.deposite(name, 1000);
+		}
+	}
+
+}
