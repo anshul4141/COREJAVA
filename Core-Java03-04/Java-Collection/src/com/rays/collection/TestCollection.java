45078e591422320dc6d41725c47d914a83bf5977
anshul4141 <anshulprajapati691@gmail.com>
2025-05-22 16:01:36 +0530

    done

---
+++ Core-Java03-04/Java-Collection/src/com/rays/collection/TestCollection.java
@@ -0,0 +1,30 @@
+package com.rays.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestCollection {
+
+	public static void main(String[] args) {
+
+		Collection c = new ArrayList();
+
+		c.add("KGF");
+		c.add('a');
+		c.add(23);
+		c.add(2.3);
+		c.add(true);
+		c.add("KGF");
+
+		System.out.println("c: " + c);
+		System.out.println("size of c: " + c.size());
+
+		System.out.println("---------------");
+
+		for (Object o : c) {
+			System.out.println(o);
+		}
+
+	}
+
+}
