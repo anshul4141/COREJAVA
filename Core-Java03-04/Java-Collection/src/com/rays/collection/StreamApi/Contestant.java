210b63888fa8c00fb96b8173a8e60cb9b1ecc8dd
anshul4141 <anshulprajapati691@gmail.com>
2025-06-05 16:06:32 +0530

    done

---
+++ Core-Java03-04/Java-Collection/src/com/rays/collection/StreamApi/Contestant.java
@@ -0,0 +1,13 @@
+package com.rays.collection.StreamApi;
+
+public class Contestant {
+
+	public String phone;
+	public String name;
+
+	public Contestant(String phone, String name) {
+		this.phone = phone;
+		this.name = name;
+	}
+
+}
