1e422d7db6cea6e2a7cac028646a42132d89ce8c
anshul4141 <anshulprajapati691@gmail.com>
2025-12-05 14:09:20 +0530

    done

---
+++ CORE-JAVA01-02/Java_Collection/src/com/rays/collection/list/TestStack.java
@@ -0,0 +1,24 @@
+package com.rays.collection.list;
+
+import java.util.Stack;
+
+// lifo last in first our
+public class TestStack {
+
+	public static void main(String[] args) {
+
+		Stack s = new Stack();
+
+		for (char c = 'a'; c <= 'z'; c++) {
+			s.push(c);
+		}
+		
+		System.out.println("stack: " + s);
+		System.out.println(s.peek());
+		System.out.println("stack: " + s);
+		System.out.println(s.pop());
+		System.out.println("stack: " + s);
+
+	}
+
+}
