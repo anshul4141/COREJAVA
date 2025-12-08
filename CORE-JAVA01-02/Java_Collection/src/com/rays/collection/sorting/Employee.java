b5e0d31ee2a59ddbfeaa3566dea8bb1d73632e2d
anshul4141 <anshulprajapati691@gmail.com>
2025-12-08 14:00:14 +0530

    done

---
+++ CORE-JAVA01-02/Java_Collection/src/com/rays/collection/sorting/Employee.java
@@ -0,0 +1,20 @@
+package com.rays.collection.sorting;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private int salary;
+
+	public Employee(int id, String name, int salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "id: " + id + " name: " + name + " salary: " + salary;
+	}
+
+}
