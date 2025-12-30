d19f5e68cfdd447d159c3d440d27f99a35cdab00
anshul4141 <anshulprajapati691@gmail.com>
2025-12-30 11:20:28 +0530

    done

--- Java_Basic/src/com/rays/string/TestStringBuilder.java
+++ Java_Basic/src/com/rays/string/TestStringBuilder.java
@@ -2,4 +2,13 @@ package com.rays.string;
 
 public class TestStringBuilder {
 
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder("sunrays");
+		System.out.println(sb);
+		System.out.println(sb.append(" house"));
+		System.out.println(sb);
+
+	}
+
 }
