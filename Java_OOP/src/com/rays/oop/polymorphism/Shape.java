85ead3d6655fbac179ed51dc4366d30737f87199
anshul4141 <anshulprajapati691@gmail.com>
2026-01-05 13:14:13 +0530

    done

--- Java_OOP/src/com/rays/oop/polymorphism/Shape.java
+++ Java_OOP/src/com/rays/oop/polymorphism/Shape.java
@@ -25,4 +25,20 @@ public class Shape {
 		return 0.0;
 	}
 
+	public static Shape getShape(int i) {
+		if (i == 1) {
+			return new Circle();
+		}
+
+		if (i == 2) {
+			return new Rectangle();
+		}
+
+		if (i == 3) {
+			return new Triangle();
+		}
+
+		return new Shape();
+	}
+
 }
